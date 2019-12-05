08d7ae9d9365629dacc50d5c6910ff35f839a693
Manuel Baun <manuel.baun@gmail.com>
2019-12-05 10:08:19 +0100

    refactor message style

--- src/components/messages/messageRow.tsx
+++ src/components/messages/messageRow.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Message from '../../models/message';
 
 import './message.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function addZero(t: number | string) {
 	if (t < 10) t = '0' + t;
@@ -22,21 +25,34 @@ const MessageRow: React.FC<MessageRowProps> = (props) => {
 	if (h > 12) h -= 12;
 	else if (h === 0) h = 12;
 
-	const layoutClass = incoming ? 'incoming' : 'outgoing';
 	const time = `${h}:${m}:${s}`;
 
-	return (
-		<div className="box-wrapper">
-			<div className={'box ' + layoutClass}>
-				<div className="message-details">
-					<span>{time}</span>
-					--
-					<span> {message.user} </span>
-				</div>
-				<div>{message.message}</div>
+	if (incoming) {
+		return (
+			<Container className="box-wrapper">
+				<Row className={'box incoming'}>
+					<Col className="message-details">
+						<Row className="user"> {message.user} </Row>
+						<Row className="time">{time}</Row>
+					</Col>
+					<Col className="message">{message.message}</Col>
+				</Row>
+			</Container>
+		);
+	} else {
+		return (
+			<div className="box-wrapper">
+				{/* class outgoing is !important */}
+				<Row className={'box outgoing'}>
+					<Col className="message">{message.message}</Col>
+					<Col className="message-details">
+						<Row className="user"> {message.user} </Row>
+						<Row className="time">{time}</Row>
+					</Col>
+				</Row>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default MessageRow;
