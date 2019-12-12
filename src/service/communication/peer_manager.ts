5c4cfb608f9705005a13c0d8bb2311ee9d0a7061
Manuel Baun <manuel.baun@gmail.com>
2019-12-12 17:59:33 +0100

    tested function type declaration and check if functions are defined

--- src/service/communication/peer_manager.ts
+++ src/service/communication/peer_manager.ts
@@ -10,6 +10,10 @@ import { Data, ConnectionData, ConnectionEventType, DataRaw } from './communicat
  * sendData
  */
 
+// Function type declaration
+type FunctionOnData = (data: Data) => void;
+type FunctionOnConnection = (data: ConnectionData) => void;
+
 class PeerManager extends Peer {
 	constructor(peerJSOptions?: Peer.PeerJSOption) {
 		super('', peerJSOptions);
@@ -23,8 +27,10 @@ class PeerManager extends Peer {
 	}
 
 	private localID: string;
-	private listOfAllPeers: string[] = new Array();
+	private listOfAllPeers: string[] = [];
 	private allConnections: Map<string, Peer.DataConnection> = new Map();
+	private onData: FunctionOnData;
+	private onConnection: FunctionOnConnection;
 
 	private onPeerOpen(id: string): void {
 		// Workaround for peer.reconnect deleting previous id
@@ -59,7 +65,7 @@ class PeerManager extends Peer {
 		console.error(err);
 	}
 
-	private connectTo(peerID) {
+	private connectTo(peerID): void {
 		// catch if it is the own peerID
 		if (peerID === this.id) return;
 
@@ -93,40 +99,37 @@ class PeerManager extends Peer {
 		this.allConnections.set(conn.peer, conn);
 	}
 
-	private onConnectionOpen(conn: Peer.DataConnection) {
+	private onConnectionOpen(conn: Peer.DataConnection): void {
 		const { peer: id } = conn;
 		console.log('Connected to: ', id);
 
 		// emit event of new Connection
-		this.onConnection({ type: ConnectionEventType.OPEN, peerID: id });
+		this.onConnection && this.onConnection({ type: ConnectionEventType.OPEN, peerID: id });
 	}
 
-	private onConnectionData(conn: Peer.DataConnection, dataRaw: DataRaw) {
+	private onConnectionData(conn: Peer.DataConnection, dataRaw: DataRaw): void {
 		// console.log('Data from:', conn.peer, dataRaw);
 		// emit event of incoming data
 		const { type } = dataRaw;
 		const payload = new Uint8Array(dataRaw.payload);
-		this.onData({ type, payload });
+		this.onData && this.onData({ type, payload });
 	}
 
-	private onConnectionClose(conn: Peer.DataConnection) {
+	private onConnectionClose(conn: Peer.DataConnection): void {
 		const { peer: id } = conn;
 		console.log('Connection closed');
 		this.allConnections.delete(id);
 		this.allConnections[id] = null;
-		this.onConnection({ type: ConnectionEventType.CLOSE, peerID: id });
+		this.onConnection && this.onConnection({ type: ConnectionEventType.CLOSE, peerID: id });
 	}
 
-	onData = (data: Data): void => {};
-	onConnection = (data: ConnectionData): void => {};
-
-	send(data: Data) {
+	send(data: Data): void {
 		this.allConnections.forEach((connection) => {
 			connection.send(data);
 		});
 	}
 
-	sendDataToID(id: string, data: Data) {
+	sendDataToID(id: string, data: Data): void {
 		const conn = this.allConnections.get(id);
 		if (conn) conn.send(data);
 		else console.log('No connection to peer with id', id);
