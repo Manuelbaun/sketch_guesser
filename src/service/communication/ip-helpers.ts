bedcfb82a4091bbc5e040d2d26f4ef6c2d9a696e
Manuel Baun <manuel.baun@gmail.com>
2020-01-22 22:22:49 +0100

    changed ip address url

--- src/service/communication/ip-helpers.ts
+++ src/service/communication/ip-helpers.ts
@@ -2,7 +2,6 @@
 // to see if clients are actually on different public ips
 var xhr = new XMLHttpRequest();
 
-
 // For some reason, getting the actual IP-Address
 // does not work in the brave browser
 export function getPublicIpAddress() {
@@ -15,14 +14,14 @@ export function getPublicIpAddress() {
 			// Process our return data
 			if (xhr.status >= 200 && xhr.status < 300) {
 				// What do when the request is successful
-				const data = JSON.parse(xhr.response);
-				resolve({ code: xhr.status, msg: data.ip });
+
+				resolve({ code: xhr.status, msg: xhr.response });
 			} else {
 				// What to do when the request has failed
 				reject({ code: '0', msg: xhr });
 			}
 		};
-		xhr.open('GET', 'https://api.ipify.org?format=json');
+		xhr.open('GET', 'http://ipinfo.io/ip');
 		xhr.send();
 	});
 }
