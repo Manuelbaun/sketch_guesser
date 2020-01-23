fa459042076b484bf2d8f1ca30faee76a9d767c0
Manuel Baun <manuel.baun@gmail.com>
2020-01-23 13:21:18 +0100

    replaced switchstatment with if, elseif , because of compile warnings of missing default

--- src/service/communication/y-webrtc.js
+++ src/service/communication/y-webrtc.js
@@ -499,66 +499,57 @@ export class SignalingConn extends ws.WebsocketClient {
 			rooms.forEach((room) => publishSignalingMessage(this, room, { type: 'announce', from: room.peerId }));
 		});
 		this.on('message', (m) => {
-			switch (m.type) {
-				case 'publish': {
-					const roomName = m.topic;
-					const room = rooms.get(roomName);
-					if (room == null || typeof roomName !== 'string') {
+			if (m.type === 'publish') {
+				const roomName = m.topic;
+				const room = rooms.get(roomName);
+				if (room == null || typeof roomName !== 'string') {
+					return;
+				}
+				const execMessage = (data) => {
+					const webrtcConns = room.webrtcConns;
+					const peerId = room.peerId;
+					if (
+						data == null ||
+						data.from === peerId ||
+						(data.to !== undefined && data.to !== peerId) ||
+						room.bcConns.has(data.from)
+					) {
+						// ignore messages that are not addressed to this conn, or from clients that are connected via broadcastchannel
 						return;
 					}
-					const execMessage = (data) => {
-						const webrtcConns = room.webrtcConns;
-						const peerId = room.peerId;
-						if (
-							data == null ||
-							data.from === peerId ||
-							(data.to !== undefined && data.to !== peerId) ||
-							room.bcConns.has(data.from)
-						) {
-							// ignore messages that are not addressed to this conn, or from clients that are connected via broadcastchannel
-							return;
-						}
-						const emitPeerChange = webrtcConns.has(data.from)
-							? () => {}
-							: () =>
-									room.provider.emit('peers', [
-										{
-											removed     : [],
-											added       : [ data.from ],
-											webrtcPeers : Array.from(room.webrtcConns.keys()),
-											bcPeers     : Array.from(room.bcConns)
-										}
-									]);
-						switch (data.type) {
-							case 'announce':
-								map.setIfUndefined(
+					const emitPeerChange = webrtcConns.has(data.from)
+						? () => {}
+						: () =>
+								room.provider.emit('peers', [
+									{
+										removed     : [],
+										added       : [ data.from ],
+										webrtcPeers : Array.from(room.webrtcConns.keys()),
+										bcPeers     : Array.from(room.bcConns)
+									}
+								]);
+					if (data.type === 'announce') {
+						map.setIfUndefined(webrtcConns, data.from, () => new WebrtcConn(this, true, data.from, room));
+						emitPeerChange();
+					} else if (data.type === 'signal') {
+						if (data.to === peerId) {
+							map
+								.setIfUndefined(
 									webrtcConns,
 									data.from,
-									() => new WebrtcConn(this, true, data.from, room)
-								);
-								emitPeerChange();
-								break;
-							case 'signal':
-								if (data.to === peerId) {
-									map
-										.setIfUndefined(
-											webrtcConns,
-											data.from,
-											() => new WebrtcConn(this, false, data.from, room)
-										)
-										.peer.signal(data.signal);
-									emitPeerChange();
-								}
-								break;
+									() => new WebrtcConn(this, false, data.from, room)
+								)
+								.peer.signal(data.signal);
+							emitPeerChange();
 						}
-					};
-					if (room.key) {
-						if (typeof m.data === 'string') {
-							cryptoutils.decryptJson(buffer.fromBase64(m.data), room.key).then(execMessage);
-						}
-					} else {
-						execMessage(m.data);
 					}
+				};
+				if (room.key) {
+					if (typeof m.data === 'string') {
+						cryptoutils.decryptJson(buffer.fromBase64(m.data), room.key).then(execMessage);
+					}
+				} else {
+					execMessage(m.data);
 				}
 			}
 		});
